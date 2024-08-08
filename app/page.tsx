a13dcbb1176a8ee34f5b9c7e42c7f144b5c99a99
Haruka Ogino <harukariq@gmail.com>
2024-08-09 09:09:49 +1200

    style buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
+'use client'
+
+import { useState } from 'react'
+
 export default function Home() {
+  const [response, setResponse] = useState(false)
+
+  function handleSubmti() {
+    setResponse(true)
+  }
+
   return (
     <main className="flex min-h-screen flex-row items-center justify-around p-24">
-      <form className="flex flex-col">
+      <form className="flex flex-col" onSubmit={handleSubmti}>
         <label htmlFor="location">Location: </label>
         <input
           type="text"
           name="location"
           id="location"
+          className="mb-2 border border-2px border-green"
           // placeholder="Enter the location of your project"
         />
         <label htmlFor="type">Type of Plant: </label>
@@ -14,6 +25,7 @@ export default function Home() {
           type="text"
           name="type"
           id="type"
+          className="mb-2 border border-2px border-green"
           // placeholder="Plant type"
         />
         <label htmlFor="accessibility">
@@ -23,14 +35,21 @@ export default function Home() {
           type="text"
           name="accessibility"
           id="accessibility"
+          className="mb-2 border border-2px border-green"
           // placeholder="Access to plant"
         />
-        <button type="submit">Submit</button>
+        <button
+          type="submit"
+          className="button-3"
+          // onClick={() => setResponse(true)}
+        >
+          Submit
+        </button>
       </form>
-      <div className="flex flex-col justify-center border border-2px border-black bg-slate-300 h-96 w-96 text-center text-2xl">
+      <div className="flex flex-col justify-center border border-2px border-black h-96 w-96 text-center text-2xl">
         <label htmlFor="requirements">Upload project requirements</label>
         <input
-          className="flex flex-col justify-center items-center"
+          className="ml-8"
           type="file"
           name="requirements"
           id="requirements"
